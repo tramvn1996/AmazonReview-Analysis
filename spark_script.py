d9ef2bb0fb844d1c0b516cfc2dbb904c32ef8f19
Tram Nguyen <tramvn1996@gmail.com>
2018-04-12 13:26:27 -0400

    add working file and result

--- spark_script.py
+++ spark_script.py
@@ -1,9 +1,9 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import exp
+from pyspark.sql.functions import desc
 
 spark = SparkSession.builder.appName("amazon reviews").getOrCreate()
-#merged = spark.read.json("file:///bigdata/data/amazon-reviews/merged.json").rdd
-merged = spark.read.json("file:///home/tnguyen/BigData/project2/small.json").rdd
+merged = spark.read.json("file:///bigdata/data/amazon-reviews/merged.json").rdd
+#merged = spark.read.json("file:///home/tnguyen/BigData/project2/small.json").rdd
 
 
 
@@ -15,31 +15,34 @@ def cats(review):
                 for cat2 in cat:
                     s.append((cat2, 1))
     return s
-count = merged.flatMap(cats)
+#count = merged.flatMap(cats)
 #print(count.collect())
 
-countrow=count.reduceByKey(lambda accum, new: accum+new)
+#countrow=count.reduceByKey(lambda accum, new: accum+new)
 #print(countrow.collect())
-count_df = countrow.toDF(['key','count'])
+#count_df = countrow.toDF(['key','count'])
 def cats_review(review):
     s=[]
     if review['categories'] is not None:
         for cat in review['categories']:
             if cat is not None:
                 for cat2 in cat:
-                    s.append((cat2,review['overall']))
+                    s.append((cat2,review['overall'],1))
     return s
 rank = merged.flatMap(cats_review)
 #rank = merged.flatMap(lambda review: (cat, review['overall']) for cat in review['categories'])
-print(rank.collect())
-summ = rank.reduceByKey(lambda accum, new: accum+new)
-summ_df = summ.toDF(['key','summ'])
+#summ = rank.reduceByKey(lambda accum, new: accum+new)
+summ_df = rank.toDF(['key','summ','count'])
+new_df = summ_df.groupby("key").sum()
+#new_df = new_df.toDF(['key','summ','count'])
 #summ = rank.reduceByKey(lambda (accumsum, accumcount), (newsum, newcount): (accumsum+newsum,accumcount+accumcount+newcount))
-avg = summ.join(countrow)
-avg_df = summ_df.join(count_df, summ_df.key==count_df.key,'outer')
-avg_df.show(n=2)
-av = avg_df['summ']/avg_df['count']
-avg_2 = avg_df.withColumn('total', av)
-avg_2.toPandas().to_csv('/home/tnguyen/BigData/project2/result.csv')
+#avg = summ.join(countrow)
+#avg_df = summ_df.join(count_df, summ_df.key==count_df.key,'outer')
+#avg_df.show(n=2)
+av = new_df['sum(summ)']/new_df['sum(count)']
+avg_2 = new_df.withColumn('total', av)
+avg_3 = avg_2.orderBy(desc("total"))
+#avg_2new_df.show()
+avg_3.toPandas().to_csv('bigresult.csv')
 
-avg_2.show()
+#print(summ.collect())
